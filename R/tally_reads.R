990020561ff110a48e6372142a6ba75b7eb7b8be
John Favate <john.favate@rutgers.edu>
2021-10-04 10:09:06 -0400

    Updated tally_Reads to use length specific offsets

--- R/tally_reads.R
+++ R/tally_reads.R
@@ -1,23 +1,23 @@
 #' Count reads at positions given an offset
 #'
 #' This function counts the number of reads with 3' ends at a given position and
-#' separates them by read lengths. This function defaults to using 4 threads. IT
-#' CURRENTLY ONLY CONSIDERS POSITIVE STRAND READS.
+#' separates them by read lengths.
 #'
 #' @param bamList A named list of bam files, presumably from the remove_contam_reads function
-#' @param offset The offset from the 3' end, this is applied to all reads of all lengths
+#' @param offset A data frame of columns qwidth, offset, specifying the read length specific offsets
 #' @returns A single data frame with counts per position per read length for all samples
 #' @export
 #'
-tally_reads <- function(bamList, offset, ncores = 4){
-  parallel::mclapply(bamList, function(bam){      # for each bam
-    bam |>                                        #
-      tibble::as_tibble() |>                      # convert to df
+tally_reads <- function(bamList, offset, ncores){
+  parallel::mclapply(bamList, function(bam){        # for each bam
+    bam |>                                          #
+      tibble::as_tibble() |>                        # convert to df
+      dplyr::left_join(., offset, by = "qwidth") |> # join the offset data frame
       dplyr::mutate(position = ifelse(strand == "+", end - offset, start + offset)) |>
-      dplyr::group_by(strand, qwidth, position) |>            # for each end and read length
-      dplyr::tally() |>                          # count reads that occur there
-      dplyr::ungroup() |>                        #
-      dplyr::mutate(rpm = n*1e6/sum(n))
-  }, mc.cores = ncores) |>                        # number of cores
-    dplyr::bind_rows(.id = "sample")              # combine to one df
+      dplyr::group_by(strand, qwidth, position) |>  # for each end and read length
+      dplyr::tally() |>                             # count reads that occur there
+      dplyr::ungroup() |>                           #
+      dplyr::mutate(rpm = n*1e6/sum(n))             # normalize reads
+  }, mc.cores = ncores) |>                          # number of cores
+    dplyr::bind_rows(.id = "sample")                # combine to one df
 }
